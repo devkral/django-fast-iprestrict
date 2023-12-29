00536fdb31c354a9b41d0487741ec3363074e886
alex <devkral@web.de>
2023-12-29 17:10:22 +0100

    add security feature to validator

--- django_fast_iprestrict/validators.py
+++ django_fast_iprestrict/validators.py
@@ -24,5 +24,9 @@ def validate_regex(value):
 
 def validate_generator_fn(value):
     # TODO: expand with security checks
-    if not value.replace(".", "").isidentifier():
+    if not all(lambda x: x.isidentifier(), value.split(".", "")):
         raise ValidationError("Invalid path.", code="invalid", params={"value": value})
+    if not value.endswith("iprestrict_gen"):
+        raise ValidationError(
+            "not a valid generator function.", code="invalid", params={"value": value}
+        )
