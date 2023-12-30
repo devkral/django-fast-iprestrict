b16fd387a9c9d05d031da456f3c8ec756e7f13fb
alex <devkral@web.de>
2023-12-30 07:40:44 +0100

    fix tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,22 +3,23 @@ from django.test import TestCase
 
 admin_index_pages = [
     "/admin/django_fast_iprestrict/",
-    "/admin/django_fast_iprestrict/rulenetwork",
-    "/admin/django_fast_iprestrict/rulepath",
-    "/admin/django_fast_iprestrict/rulesource",
-    "/admin/django_fast_iprestrict/rule",
+    "/admin/django_fast_iprestrict/rulenetwork/",
+    "/admin/django_fast_iprestrict/rulepath/",
+    "/admin/django_fast_iprestrict/rulesource/",
+    "/admin/django_fast_iprestrict/rule/",
 ]
 
 
 class SyncTests(TestCase):
     def setUp(self):
-        self.admin_user = User.objects.create_superuser(name="admin")
+        self.admin_user = User.objects.create_superuser(username="admin")
 
     def test_admin(self):
         self.client.force_login(self.admin_user)
         for page in admin_index_pages:
-            response = self.client.get(page)
-            self.assertEqual(response.status, 200)
+            with self.subTest("subtest for: %(page)s", page=page):
+                response = self.client.get(page)
+                self.assertEqual(response.status_code, 200)
 
 
 class AsyncTests(TestCase):
